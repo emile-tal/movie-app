2c4397dc720dcfc5ed457050250c41cc9a2396cc
Emile <emiletal@gmail.com>
2025-06-14 22:20:21 -0400

    Style home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <SearchBar placeholder="Search" onPress={() => router.push("/search")} />
         </View>
       </ScrollView>
     </View>
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,16 +2,17 @@ import { icons } from "@/constants/icons";
 import { Image, TextInput, View } from "react-native";
 
 interface SearchBarProps {
-    onPress: () => void;
+    placeholder: string;
+    onPress?: () => void;
 }
 
-export default function SearchBar({ onPress }: SearchBarProps) {
+export default function SearchBar({ placeholder, onPress }: SearchBarProps) {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
                 onPress={onPress}
-                placeholder="Search"
+                placeholder={placeholder}
                 value=""
                 onChangeText={() => { }}
                 placeholderTextColor="#a8b5db"
