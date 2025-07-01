07e73ec79dc14c61bb325656428976e3f12f175f
Emile <emiletal@gmail.com>
2025-06-30 20:06:48 -0400

    Hook up appwrite backend & database

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -4,19 +4,21 @@ import { Image, TextInput, View } from "react-native";
 interface SearchBarProps {
     placeholder: string;
     onPress?: () => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 
-export default function SearchBar({ placeholder, onPress }: SearchBarProps) {
+export default function SearchBar({ placeholder, onPress, value, onChangeText }: SearchBarProps) {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=""
-                onChangeText={() => { }}
+                value={value || ''}
+                onChangeText={onChangeText || (() => { })}
                 placeholderTextColor="#a8b5db"
-                className="flex-1 ml-2"
+                className="flex-1 ml-2 text-white"
             />
         </View>
     )
---
+++ app/services/appwrite.ts
@@ -0,0 +1,49 @@
+import { Movie } from "@/interfaces/interfaces";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_COLLECTION_ID!;
+
+const client = new Client()
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    console.log(query, movie)
+    try {
+        const result = await database.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID,
+            [Query.equal("search_term", query)]
+        )
+        if (result.documents.length > 0) {
+            const existingMovie = result.documents[0];
+
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                existingMovie.$id,
+                {
+                    count: existingMovie.count + 1
+                }
+            )
+        } else {
+            await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    search_term: query,
+                    count: 1,
+                    movie_id: movie.id,
+                    poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                    title: movie.title,
+                }
+            )
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
